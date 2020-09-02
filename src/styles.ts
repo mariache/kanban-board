502d538ec8f0c64a6aba870ad6ba8acf0ddf9792
Salam Omarbagaev <omarbagaev@gmail.com>
2020-09-02 23:14:29 +1200

    -styles for button; fom; input;

--- src/styles.ts
+++ src/styles.ts
@@ -1,5 +1,9 @@
 import styled from "styled-components";
 
+interface AddItemButtonProps {
+  dark?: boolean;
+}
+
 export const AppContainer = styled.div`
   align-items: flex-start;
   background-color: #3179ba;
@@ -33,4 +37,47 @@ export const CardContainer = styled.div`
   margin-bottom: 0.5rem;
   max-width: 300px;
   box-shadow: #091e4240 0px 1px 0px 0px;
+`;
+
+export const AddItemButton = styled.div<AddItemButtonProps>`
+  background-color: #ffffff3d;
+  border-radius: 3px;
+  border: none;
+  color: ${(props) => (props.dark ? "#000" : "#fff")};
+  cursor: pointer;
+  max-width: 300px;
+  padding: 10px 12px;
+  text-align: left;
+  transition: background 85ms ease-in;
+  width: 100%;
+  &:hover {
+    background-color: #ffffff52;
+  }
+`;
+
+export const NewItemFormContainer = styled.div`
+  max-width: 300px;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  align-items: flex-start;
+`;
+
+export const NewItemButton = styled.div`
+  background-color: #5aac44;
+  border-radius: 3px;
+  border: none;
+  box-shadow: none;
+  color: #fff;
+  padding: 6px 12px;
+  text-align: center;
+`;
+
+export const NewItemInput = styled.div`
+  border-radius: 3px;
+  border: none;
+  box-shadow: #091e4240 0px 1px 0px 0px;
+  margin-bottom: 0.5rem;
+  padding: 0.5rem 1rem;
+  width: 100%;
 `;
