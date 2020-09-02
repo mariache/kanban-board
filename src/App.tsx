bfab388e1dfb90f05cf7b0d2029d674a6e669281
Salam Omarbagaev <omarbagaev@gmail.com>
2020-09-02 22:49:48 +1200

    - render columns to the app;

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
 import React from "react";
 import { AppContainer } from "./styles";
+import { Column } from "./Column";
+import { Card } from "./Card";
 
 function App() {
-  return <AppContainer>Columns go here</AppContainer>;
+  return (
+    <AppContainer>
+      <Column text="To Do">
+        <Card text="Generate scaffolding" />
+      </Column>
+      <Column text="In Progress">
+        <Card text="Learn Typescript" />
+      </Column>
+      <Column text="Done">
+        <Card text="Begin to se static typing" />
+      </Column>
+    </AppContainer>
+  );
 }
 
 export default App;
