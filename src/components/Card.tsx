6b4aaaa5bcedf83a99f8fc5460d6334289d6273c
Salam Omarbagaev <omarbagaev@gmail.com>
2020-09-23 22:54:39 +1200

    - draggable columns;

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,6 +3,7 @@ import { CardContainer } from "./styles";
 
 interface CardProps {
   text: string;
+  index: number;
 }
 
 export const Card: React.FC<CardProps> = ({ text }: CardProps) => {
--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useRef } from "react";
 import { ColumnContainer, ColumnTitle } from "./styles";
 import { AddNewItem } from "./AddNewItem";
 import { useAppState } from "../context/AppStateContext";
 import { Card } from "./Card";
-import { ADD_TASK } from "../context/constants";
+import { ADD_TASK, MOVE_LIST, COLUMN } from "../context/constants";
+import { useItemDrag } from "../utils/useItemDrag";
+import { useDrop } from "react-dnd";
+import { DragItem } from "../DragItem";
+import { isHidden } from "../utils/isHidden";
 
 interface ColumnProps {
   text: string;
@@ -11,18 +15,38 @@ interface ColumnProps {
   id: string;
 }
 
-export const Column: React.FC<ColumnProps> = ({
-  text,
-  index,
-  id,
-}: React.PropsWithChildren<ColumnProps>) => {
+export const Column = ({ text, index, id }: ColumnProps) => {
   const { state, dispatch } = useAppState();
+  const ref = useRef<HTMLDivElement>(null);
+  const [, drop] = useDrop({
+    accept: COLUMN,
+    hover(item: DragItem) {
+      if (item.type === COLUMN) {
+        const dragIndex = item.index;
+        const hoverIndex = index;
+
+        if (dragIndex === hoverIndex) {
+          return;
+        }
+
+        dispatch({ type: MOVE_LIST, payload: { dragIndex, hoverIndex } });
+        item.index = hoverIndex;
+      }
+    },
+  });
+
+  const { drag } = useItemDrag({ type: COLUMN, id, index, text });
+
+  drag(drop(ref));
 
   return (
-    <ColumnContainer>
+    <ColumnContainer
+      ref={ref}
+      isHidden={isHidden(state.draggedItem, COLUMN, id)}
+    >
       <ColumnTitle>{text}</ColumnTitle>
-      {state.lists[index].tasks.map((task) => (
-        <Card text={task.text} key={task.id} />
+      {state.lists[index].tasks.map((task, idx) => (
+        <Card text={task.text} key={task.id} index={idx} />
       ))}
       <AddNewItem
         toggleButtonText="+ Add another card"
