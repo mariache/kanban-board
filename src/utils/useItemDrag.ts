4f902bce0e4f57f676d9d7663c183d1af2e5805d
Salam Omarbagaev <omarbagaev@gmail.com>
2020-09-27 22:57:44 +1300

    - changes in column;

--- src/utils/useItemDrag.ts
+++ src/utils/useItemDrag.ts
@@ -1,11 +1,13 @@
-import { useDrag } from "react-dnd";
-import { useAppState } from "../context/AppStateContext";
 import { DragItem } from "../DragItem";
+import { useEffect } from "react";
+import { useAppState } from "../context/AppStateContext";
+import { useDrag } from "react-dnd";
+import { getEmptyImage } from "react-dnd-html5-backend";
 import { SET_DRAGGED_ITEM } from "../context/constants";
 
 export const useItemDrag = (item: DragItem) => {
   const { dispatch } = useAppState();
-  const [, drag] = useDrag({
+  const [, drag, preview] = useDrag({
     item,
     begin: () =>
       dispatch({
@@ -14,5 +16,8 @@ export const useItemDrag = (item: DragItem) => {
       }),
     end: () => dispatch({ type: SET_DRAGGED_ITEM, payload: undefined }),
   });
+  useEffect(() => {
+    preview(getEmptyImage(), { captureDraggingState: true });
+  }, [preview]);
   return { drag };
 };
