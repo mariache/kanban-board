7429c4307221acab14cf176605f1fa72a88b8bc8
Salam Omarbagaev <omarbagaev@gmail.com>
2020-10-03 21:13:11 +1300

    - MOVE_TASK action;

--- src/context/AppStateContext.tsx
+++ src/context/AppStateContext.tsx
@@ -1,6 +1,12 @@
 import { AppState } from "../types";
 import React, { useContext, useReducer, createContext } from "react";
-import { ADD_LIST, ADD_TASK, MOVE_LIST, SET_DRAGGED_ITEM } from "./constants";
+import {
+  ADD_LIST,
+  ADD_TASK,
+  MOVE_LIST,
+  SET_DRAGGED_ITEM,
+  MOVE_TASK,
+} from "./constants";
 import { nanoid } from "nanoid";
 import { appData } from "../testData";
 import { findItemIndexById } from "../utils/findItemIndexById";
@@ -31,6 +37,15 @@ type Action =
   | {
       type: "SET_DRAGGED_ITEM";
       payload: DragItem | undefined;
+    }
+  | {
+      type: "MOVE_TASK";
+      payload: {
+        dragIndex: number;
+        hoverIndex: number;
+        sourceColumn: string;
+        targetColumn: string;
+      };
     };
 
 const appStateReducer = (state: AppState, action: Action): AppState => {
@@ -64,6 +79,19 @@ const appStateReducer = (state: AppState, action: Action): AppState => {
     }
     case SET_DRAGGED_ITEM: {
       return { ...state, draggedItem: action.payload };
+    }
+    case MOVE_TASK: {
+      const {
+        dragIndex,
+        hoverIndex,
+        sourceColumn,
+        targetColumn,
+      } = action.payload;
+      const sourceLaneIndex = findItemIndexById(state.lists, sourceColumn);
+      const targetLaneIndex = findItemIndexById(state.lists, targetColumn);
+      const item = state.lists[sourceLaneIndex].tasks.splice(dragIndex, 1)[0];
+      state.lists[targetLaneIndex].tasks.splice(hoverIndex, 0, item);
+      return { ...state };
     }
     default: {
       return state;
--- src/context/constants.ts
+++ src/context/constants.ts
@@ -2,4 +2,5 @@ export const ADD_LIST = "ADD_LIST";
 export const ADD_TASK = "ADD_TASK";
 export const MOVE_LIST = "MOVE_LIST";
 export const SET_DRAGGED_ITEM = "SET_DRAGGED_ITEM";
+export const MOVE_TASK = "MOVE_TASK";
 export const COLUMN = "COLUMN";
